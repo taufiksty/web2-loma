503fe053b0e0e29884b88ec54df25888104389b4
syalaishasauma03 <syalaishasauma@gmail.com>
2022-10-27 13:06:33 +0700

    test

--- app/Views/landing_page.php
+++ app/Views/landing_page.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Loma | Lowongan Mahasiswa</title>
+  <title>Loma | Lowongan</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/daisyui@2.31.0/dist/full.css" type="text/css" />
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
