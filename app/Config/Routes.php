032cc0fb16dde26192dc56232ebd80a57742b31a
taufiksty <taufik.satya09@gmail.com>
2022-11-29 17:37:28 +0700

    try to set role user

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -38,10 +38,17 @@ $routes->setAutoRoute(true);
 $routes->get('/', 'LandingPage::index');
 $routes->get('/login', 'Auth::login');
 $routes->get('/register', 'Auth::register');
-$routes->get('/pelamar/(:num)', 'Pelamar::index/$1');
-$routes->get('/rekruter/(:num)', 'Rekruter::index/$1');
-$routes->get('/magang', 'Magang::index');
-$routes->get('/magang/(:num)', 'Magang::detailLowongan/$1');
+
+$routes->group('pelamar', ['filter' => 'role:pelamar'], function($routes) {
+    $routes->get('pelamar/(:num)', 'Pelamar::index/$1');
+    $routes->post('pelamar/editprofil/(:num)', 'Pelamar::editProfil/$1');
+});
+
+
+// $routes->get('/pelamar/(:num)', 'Pelamar::index/$1');
+// $routes->get('/rekruter/(:num)', 'Rekruter::index/$1');
+// $routes->get('/magang', 'Magang::index');
+// $routes->get('/magang/(:num)', 'Magang::detailLowongan/$1');
 $routes->get('/lamar/(:any)/(:num)', 'Pelamar::lamar/$1/$2');  
 
 /*
